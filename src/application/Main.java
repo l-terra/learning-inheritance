d46aec74b06f9f061c5ddea84aa9ef422530765a
Lucas Terra <lucas.terra8587@gmail.com>
2025-02-04 12:11:05 -0300

    abstract classes

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,9 +1,10 @@
 package application;
 
-import entities.Account;
-import entities.BusinessAccount;
-import entities.SavingsAccount;
+import entities.*;
 
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 public class Main {
@@ -11,18 +12,34 @@ public class Main {
 
         Locale.setDefault(Locale.US);
 
-        Account acc1 = new Account(1001, "Alex", 1000.0);
-        acc1.withdraw(200.0);
-        System.out.println(acc1.getBalance());
+        List<Account> list = new ArrayList<>();
 
-        // Using override method
-        Account acc2 = new SavingsAccount(1002, "Maria", 1000.0, 0.01);
-        acc2.withdraw(200.0);
-        System.out.println(acc2.getBalance());
+        list.add(new SavingsAccount(1001, "Alex", 500.0, 0.01));
+        list.add(new BusinessAccount(1002, "Maria", 1000.0 , 400.0));
+        list.add(new SavingsAccount(1003, "Bob", 300.0, 0.01));
+        list.add(new BusinessAccount(1005, "Anna", 500.0, 500.0));
 
-        // Using override method with super()
-        Account acc3 = new BusinessAccount(1003, "Bob", 1000.0, 500.0);
-        acc3.withdraw(200.0);
-        System.out.println(acc3.getBalance());
+        double sum = 0.0;
+        for(Account acc : list) {
+            sum += acc.getBalance();
+        }
+
+        System.out.printf("Total balance: %.2f\n", sum);
+
+        for(Account acc : list) {
+            acc.deposit(10.00);
+        }
+
+        sum = 0.0;
+        for(Account acc : list) {
+            sum += acc.getBalance();
+        }
+
+        System.out.printf("Updated total balance: %.2f\n", sum);
+        System.out.println();
+
+        for(Account acc : list) {
+            System.out.printf("Updated balance for account %d: %.2f\n", acc.getNumber(), acc.getBalance());
+        }
     }
 }
